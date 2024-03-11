51a790d73779d9414a26506b216f40ad9be7cc3b
Daniel Ag <daniel.amarin.dag@gmail.com>
2024-03-11 14:12:48 +0100

    First commit cmake

---
+++ OpenGLV2024/OpenGLV2024.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGLV2024.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGLV2024.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
