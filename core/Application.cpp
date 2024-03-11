239de7bc98268e1094efbfa8a1cea160ce3640c1
Daniel Ag <daniel.amarin.dag@gmail.com>
2024-03-11 15:36:46 +0100

    Application class begin

---
+++ core/Application.cpp
@@ -0,0 +1,24 @@
+#include "Application.h"
+
+Application* Application::Get()
+{
+
+	return nullptr;
+}
+
+int Application::RunApplication()
+{
+	return 0;
+}
+
+void Application::InitApplication()
+{
+}
+
+void Application::InitGLFW()
+{
+}
+
+void Application::LoadContent()
+{
+}
--- core/Application.h
+++ core/Application.h
@@ -1,1 +1,34 @@
 #pragma once
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+
+///Purpose of class:
+///Create a class which controls and initialize the application.
+///   The program uses the "Singleton Pattern: 
+///   creational design pattern that ensures only a single instance of a class exists throughout an application
+///   and provides access to that instance from anywhere in the codebase".
+
+
+class Application
+{
+ ///Modifiable 
+public:
+	///Deleteing copy of constructor and "move".
+	Application(const Application&) = delete; //Constructor deleted 
+	Application(Application&&) = delete; //Move Constructor deleted 
+
+	static Application* Get(); //
+
+	int RunApplication(); 
+
+private:
+	Application() = default;
+
+	void InitApplication();
+	void InitGLFW();
+	void LoadContent();
+};
+
+
+#endif
