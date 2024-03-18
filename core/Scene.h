7075768917b2d8c4bd819961cca881752c58c1cb
Daniel Ag <daniel.amarin.dag@gmail.com>
2024-03-18 11:20:03 +0100

    More notes in Window file
    Scene class added

---
+++ core/Scene.h
@@ -0,0 +1,22 @@
+#ifndef SCENE_H
+#define SCENE_H
+#include <memory>
+#include <iostream>
+
+class Scene
+{
+public:
+	explicit Scene(const std::string& name);
+	virtual ~Scene() = default;
+
+
+
+
+
+}
+
+
+
+
+
+#endif
--- core/Window.h
+++ core/Window.h
@@ -20,19 +20,21 @@ public:
 	void StoreWindowCallback(); //Method for storing callback functions to GLFWWindow
 	bool LoadContent(); //Methods to check if loaded is content or not
 	void SetScene(Scene* scene); //Method for creating a scene. Requires: scene object with pointer.
-	void Update(float deltaTime); //Method for updating class in real time. Requires: deltaTime float value
-	void Render(float deltaTime); //Method for rendering class in real time. Requires: deltaTime float value
+	void Update(float deltaTime); //Method for updating class in real time: deltaTime float 
+	void Render(float deltaTime); //Method for rendering class in real time: deltaTime float 
 	void EndFrame(); //Method to end frame
 
 	bool IsClosed(); //Method to see if object is not loading anymore.
 
 	///Callback Methods
-	void FramebufferSizeCallback(struct GLFWwindow* window, int height, int width); //Method to create window GUI. Requires: GLFWWindow object pointer, integer for height and width
-	void MouseMovementCallback(struct GLFWwindow* window, double xPos, double yPos); //Method to create mouseMOvement.  
-	void MouseButtonCallback(struct GLFWWindow* window, int button, int action, int mods);
+	//Callback = a function passed into another function as an argument.
+	//It is then invoked inside the outer function. 
+	void FramebufferSizeCallback(struct GLFWwindow* window, int height, int width); //Method to callback window GUI: GLFWWindow object pointer, integer for height and width
+	void MouseMovementCallback(struct GLFWwindow* window, double xPos, double yPos); //Method to callback mouseMovement.  
+	void MouseButtonCallback(struct GLFWWindow* window, int button, int action, int mods); //Method  to callback MouseButton
 	void MouseScrollCallback(struct GLFWwindow* window, double xOffset, double yOffset);
 	void keyCallback(struct GLFWWindow* window, int key, int scanCode, int action, int mods);
-
+	
 
 	///Setters
 	void SetWidth(int width);
